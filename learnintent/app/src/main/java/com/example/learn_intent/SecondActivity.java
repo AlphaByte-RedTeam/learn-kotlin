20e65e9ebc19d37fb779e109944eee32187114bb
Janice Claresta Lingga <andrew.avv03@gmail.com>
2022-09-16 15:05:16 +0700

    Create Intent

--- learnintent/app/src/main/java/com/example/learn_intent/SecondActivity.java
+++ learnintent/app/src/main/java/com/example/learn_intent/SecondActivity.java
@@ -2,7 +2,10 @@ package com.example.learn_intent;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class SecondActivity extends AppCompatActivity {
 
@@ -10,5 +13,20 @@ public class SecondActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+        TextView textView = findViewById(R.id.textView);
+        Intent terima = getIntent();
+        Bundle bundle = terima.getExtras();
+
+        if (bundle != null)
+            textView.setText(bundle.getString("nama"));
+
+        Button button = findViewById(R.id.button2);
+        button.setOnClickListener(view -> {
+            Intent intent = new Intent(this, SecondActivity.class);
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            startActivity(intent);
+        });
     }
 }
