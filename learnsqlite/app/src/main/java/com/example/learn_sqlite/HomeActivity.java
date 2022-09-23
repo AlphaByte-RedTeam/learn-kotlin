5a575973d923429eafd71f2aa68cdec8fbc876c6
Janice Claresta Lingga <andrew.avv03@gmail.com>
2022-09-23 16:57:20 +0700

    dunno

--- learnsqlite/app/src/main/java/com/example/learn_sqlite/HomeActivity.java
+++ learnsqlite/app/src/main/java/com/example/learn_sqlite/HomeActivity.java
@@ -2,13 +2,79 @@ package com.example.learn_sqlite;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class HomeActivity extends AppCompatActivity {
+
+    Button logout;
+    Button mail;
+    Button phone;
+    TextView welcome;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        welcome = findViewById(R.id.txtWelcome);
+        Intent intent = getIntent();
+        Bundle bundle = intent.getExtras();
+
+        if (bundle != null) {
+            welcome.setText("Welcome " + bundle.getString("USERNAME"));
+        }
+
+        logout = findViewById(R.id.btnLogout);
+        logout.setOnClickListener(view -> {
+            Intent logoutIntent = new Intent(this, MainActivity.class);
+            logoutIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+            logoutIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            logoutIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            startActivity(logoutIntent);
+        });
+
+        mail = findViewById(R.id.btnMail);
+        mail.setOnClickListener(view -> {
+            sendMessage();
+        });
+
+        phone = findViewById(R.id.btnPhone);
+        phone.setOnClickListener(view -> {
+            makePhoneCall();
+        });
+    }
+
+    private void sendMessage() {
+        String email = "janiceclarest10@gmail.com";
+        String subject = "Hello Beb <3";
+        String message = "I love you so much!";
+
+        String mailTo =
+                "mailto:" + email +
+                "?&subject=" + Uri.encode(subject) +
+                "&body=" + Uri.encode(message);
+
+        Intent intentMail = new Intent(Intent.ACTION_SENDTO);
+        intentMail.setData(Uri.parse(mailTo));
+        try {
+            startActivity(Intent.createChooser(intentMail, "Send mail..."));
+        } catch (Exception e) {
+            Toast.makeText(this, "Exception " + e.getMessage(), Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void makePhoneCall() {
+        Intent intentPhone = new Intent(Intent.ACTION_DIAL);
+        intentPhone.setData(Uri.parse("tel: 089630497191"));
+        try {
+            startActivity(intentPhone);
+        } catch (Exception e) {
+            Toast.makeText(this, "Exception " + e.getMessage(), Toast.LENGTH_SHORT).show();
+        }
     }
 }
--- learnsqlite/app/src/main/java/com/example/learn_sqlite/RegisterActivity.java
+++ learnsqlite/app/src/main/java/com/example/learn_sqlite/RegisterActivity.java
@@ -2,13 +2,102 @@ package com.example.learn_sqlite;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.snackbar.Snackbar;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    EditText editTextUserName;
+    EditText editTextPassword;
+    EditText editTextEmail;
+    Button buttonRegister;
+    TextView textViewLogin;
+    SqliteHelper sqliteHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        sqliteHelper = new SqliteHelper(this);
+        initViews();
+        initTextViewLogin();
+        buttonRegister.setOnClickListener(view -> {
+            if (validate()) {
+                String userName = editTextUserName.getText().toString();
+                String password = editTextPassword.getText().toString();
+                String email = editTextEmail.getText().toString();
+                if (!sqliteHelper.isEmailExists(email)) {
+                    User user = new User(null, userName, email, password);
+                    sqliteHelper.addUser(user);
+                    Snackbar.make(buttonRegister, "User created successfully! Please Login ", Snackbar.LENGTH_LONG).show();
+                    new Handler().postDelayed(new Runnable() {
+                        @Override
+                        public void run() {
+                            finish();
+                        }
+                    }, Snackbar.LENGTH_LONG);
+                } else {
+                    Toast.makeText(RegisterActivity.this, "User already exists with same email ", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    private void initTextViewLogin() {
+        textViewLogin = findViewById(R.id.txtCreateAccount);
+        textViewLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(RegisterActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    private void initViews() {
+        editTextEmail = findViewById(R.id.editEmailRegis);
+        editTextPassword = findViewById(R.id.editPasswordRegis);
+        editTextUserName = findViewById(R.id.editUsernameRegis);
+        buttonRegister = findViewById(R.id.btnRegister);
+    }
+
+    public boolean validate() {
+        boolean isValidUser = false;
+
+        String userName = editTextUserName.getText().toString();
+        String password = editTextPassword.getText().toString();
+        String email = editTextEmail.getText().toString();
+
+        if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            isValidUser = false;
+            Toast.makeText(getApplicationContext(), "Please enter valid email", Toast.LENGTH_SHORT).show();
+        } else {
+            isValidUser = true;
+        }
+
+        if (password.isEmpty()) {
+            isValidUser = false;
+            Toast.makeText(getApplicationContext(), "Please enter valid password", Toast.LENGTH_SHORT).show();
+        } else {
+            isValidUser = true;
+        }
+
+        if (userName.isEmpty()) {
+            isValidUser = false;
+            Toast.makeText(getApplicationContext(), "Please enter valid username", Toast.LENGTH_SHORT).show();
+        } else {
+            isValidUser = true;
+        }
+
+        return isValidUser;
     }
 }
