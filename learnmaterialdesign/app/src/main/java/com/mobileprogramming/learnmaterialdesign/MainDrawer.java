9bfe3d4a910b5a296a779c19c3a5f0031101b09e
Andrew Virya Victorio <andrew.avv03@gmail.com>
2022-11-11 13:48:15 +0700

    feat: init commit

---
+++ learnmaterialdesign/app/src/main/java/com/mobileprogramming/learnmaterialdesign/MainDrawer.java
@@ -0,0 +1,14 @@
+package com.mobileprogramming.learnmaterialdesign;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainDrawer extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_drawer);
+    }
+}
