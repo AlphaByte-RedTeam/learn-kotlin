723fdb684d169b9451c7dd86ca62c4cc2703a21c
Andrew Virya Victorio <andrew.avv03@gmail.com>
2022-11-18 16:31:04 +0700

    idk what to commit

--- learnloginregistermysql/app/src/main/java/com/mobileprogramming/learnloginregistermysql/LoginActivity.java
+++ learnloginregistermysql/app/src/main/java/com/mobileprogramming/learnloginregistermysql/LoginActivity.java
@@ -2,13 +2,111 @@ package com.mobileprogramming.learnloginregistermysql;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
 
 public class LoginActivity extends AppCompatActivity {
+    EditText etUsername, etPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        etPassword.findViewById(R.id.editTextPassword);
+        etUsername.findViewById(R.id.editTextUsername);
+
+        findViewById(R.id.buttonLogin).setOnClickListener(view -> {
+            userLogin();
+        });
+
+        findViewById(R.id.textViewRegister).setOnClickListener(view -> {
+            startActivity(new Intent(getApplicationContext(), MainActivity.class));
+        });
+    }
+
+    private void userLogin() {
+        final String username = etUsername.getText().toString();
+        final String password = etPassword.getText().toString();
+
+        if (TextUtils.isEmpty(username)) {
+            etUsername.setError("Please enter your username");
+            etUsername.requestFocus();
+            return;
+        }
+
+        if (TextUtils.isEmpty(password)) {
+            etPassword.setError("Please enter your password");
+            etPassword.requestFocus();
+            return;
+        }
+
+        class UserLogin extends AsyncTask<Void, Void, String> {
+            ProgressBar progressBar;
+
+            @Override
+            protected void onPreExecute() {
+                super.onPreExecute();
+                progressBar = findViewById(R.id.progressBar);
+                progressBar.setVisibility(View.VISIBLE);
+            }
+
+            @Override
+            protected void onPostExecute(String s) {
+                super.onPostExecute(s);
+                progressBar.setVisibility(View.GONE);
+
+                try {
+                    JSONObject obj = new JSONObject(s);
+
+                    if (!obj.getBoolean("error")) {
+                        Toast.makeText(getApplicationContext(), obj.getString("message"), Toast.LENGTH_SHORT).show();
+
+                        JSONObject userJson = obj.getJSONObject("user");
+
+                        User user = new User(
+                                userJson.getInt("id"),
+                                userJson.getString("username"),
+                                userJson.getString("email"),
+                                userJson.getString("gender")
+                        );
+
+                        SharedPrefManager.getInstance(getApplicationContext()).userLogin(user);
+
+                        finish();
+                        startActivity(new Intent(getApplicationContext(), ProfileActivity.class));
+                    } else {
+                        Toast.makeText(getApplicationContext(), "Invalid username or password", Toast.LENGTH_SHORT).show();
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            protected String doInBackground(Void... voids) {
+                RequestHandler requestHandler = new RequestHandler();
+
+                HashMap<String, String> params = new HashMap<>();
+                params.put("username", username);
+                params.put("password", password);
+
+                return requestHandler.sendPostRequest(URLs.URL_LOGIN, params);
+            }
+        }
+
+        UserLogin ul = new UserLogin();
+        ul.execute();
     }
 }
