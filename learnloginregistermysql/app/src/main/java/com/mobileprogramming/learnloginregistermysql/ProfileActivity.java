a06486d646a059f470a7bbec67de314a4e48e9f4
Andrew Virya Victorio <andrew.avv03@gmail.com>
2022-11-18 14:15:44 +0700

    add new activity

---
+++ learnloginregistermysql/app/src/main/java/com/mobileprogramming/learnloginregistermysql/ProfileActivity.java
@@ -0,0 +1,14 @@
+package com.mobileprogramming.learnloginregistermysql;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+    }
+}
